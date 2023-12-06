11060fd56d1f93d8d8aff7425b9f876c5af0bf05
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-06 17:15:30 +0100

    implemented first get/students of all the students registered in the moc file.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { students } from "./dataBase";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.listen(3003, () => {
   console.log("Servidor rodando na porta 3003");
 });
 
-app.get("/ping", (req: Request, res: Response) => {
-  res.send("Pong!");
+//Restfull - get of students
+app.get("/students", (req: Request, res: Response) => {
+  res.status(200).send(students);
 });
