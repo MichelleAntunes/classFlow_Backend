9489b2dbf2d783b8c30b787259f04f23d611b9e0
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-06 18:28:08 +0100

    created end point for changing student information by :id

--- src/index.ts
+++ src/index.ts
@@ -62,3 +62,35 @@ app.get("/students/:id", (req: Request, res: Response) => {
   res.status(200).send(result);
   // .send('O estudante localizado foi: ${result} seu nome é ${result?.name}');
 });
+
+//edit student
+app.put("/students/:id", (req: Request, res: Response) => {
+  const idToEdit = req.params.id;
+
+  const newId = req.body.id as string | undefined;
+  const newName = req.body.name as string | undefined;
+  const newEmail = req.body.email as string | undefined;
+  const newAge = req.body.age as number | undefined;
+  const newTelephone = req.body.telepfone as number | undefined;
+  const newNotes = req.body.notes as string | undefined;
+
+  const student = students.find((student) => student.id === idToEdit);
+
+  if (student) {
+    student.id = newId || student.id;
+    student.name = newName || student.name;
+    student.email = newEmail || student.email;
+    student.age = newAge || student.age;
+    student.telepfone = newTelephone || student.telepfone;
+    student.notes = newNotes || student.notes;
+
+    // quando o valor for um número, é possível que seja 0 (que também é falsy)
+    // então para possibilitar que venha 0, podemos fazer um ternário
+    // o isNaN é uma função que checa se o argumento é um número ou não
+    // caso não seja um número o isNaN retorna true, caso contrário false
+    // por isso mantemos o antigo (pet.age) no true e atualizamos no false
+    student.age = isNaN(Number(newAge)) ? student.age : (newAge as number);
+  }
+
+  res.status(200).send("Atualização realizada com sucesso");
+});
