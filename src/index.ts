85ad7112045c8beee1fdd03aeab8a21cccd4174f
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-08 18:28:44 +0100

    Validating the data received in the backend using the type sent for each request.

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,10 @@ app.listen(3003, () => {
 app.get("/students", (req: Request, res: Response) => {
   const nameToFind = req.query.name as string;
 
+  if (typeof nameToFind !== "string") {
+    return res.status(400).send("'nameToFind' deve ser uma string");
+  }
+
   if (nameToFind) {
     const result: TStudents[] = students.filter((student) =>
       student.name.toLowerCase().includes(nameToFind.toLowerCase())
@@ -38,6 +42,25 @@ app.post("/students", (req: Request, res: Response) => {
   const email = req.body.email as string;
   const telepfone = req.body.telepfone as number;
   const notes = req.body.notes as string[];
+
+  if (typeof id !== "string") {
+    return res.status(400).send("'id' deve ser uma string");
+  }
+  if (typeof name !== "string") {
+    return res.status(400).send("'name' deve ser uma string");
+  }
+  if (typeof age !== "number") {
+    return res.status(400).send("'age' deve ser uma number");
+  }
+  if (typeof email !== "string") {
+    return res.status(400).send("'email' deve ser uma string");
+  }
+  if (typeof telepfone !== "number") {
+    return res.status(400).send("'telepfone' deve ser uma number");
+  }
+  if (typeof notes !== "string") {
+    return res.status(400).send("'notes' deve ser uma string");
+  }
 
   const newStudent: TStudents = {
     id,
@@ -57,15 +80,21 @@ app.get("/students/:id", (req: Request, res: Response) => {
   //:id poderia ser qualquer outro filtro necessário
   const idToFind = req.params.id; // não precisamos forçar a tipagem aqui, porque todo path params é string
 
+  if (typeof idToFind !== "string") {
+    return res.status(400).send("'idToFind' deve ser uma string");
+  }
+
   const result = students.find((student) => student.id === idToFind);
 
   res.status(200).send(result);
   // .send('O estudante localizado foi: ${result} seu nome é ${result?.name}');
 });
 
-//edit student
 app.put("/students/:id", (req: Request, res: Response) => {
   const idToEdit = req.params.id;
+  if (typeof idToEdit !== "string") {
+    return res.status(400).send("'id' deve ser uma string");
+  }
 
   const newId = req.body.id as string | undefined;
   const newName = req.body.name as string | undefined;
@@ -73,6 +102,25 @@ app.put("/students/:id", (req: Request, res: Response) => {
   const newAge = req.body.age as number | undefined;
   const newTelephone = req.body.telepfone as number | undefined;
   const newNotes = req.body.notes as string[] | undefined;
+
+  if (typeof newId !== "string") {
+    return res.status(400).send("'newId' deve ser uma string");
+  }
+  if (typeof newName !== "string") {
+    return res.status(400).send("'newName' deve ser uma string");
+  }
+  if (typeof newEmail !== "string") {
+    return res.status(400).send("'newEmail' deve ser uma string");
+  }
+  if (typeof newAge !== "number") {
+    return res.status(400).send("'newAge' deve ser uma number");
+  }
+  if (typeof newTelephone !== "number") {
+    return res.status(400).send("'newTelephone' deve ser uma number");
+  }
+  if (typeof newNotes !== "string") {
+    return res.status(400).send("'newNotes' deve ser uma number");
+  }
 
   const student = students.find((student) => student.id === idToEdit);
 
@@ -98,6 +146,10 @@ app.put("/students/:id", (req: Request, res: Response) => {
 app.delete("/students/:id", (req: Request, res: Response) => {
   const idToDelete = req.params.id;
 
+  if (typeof idToDelete !== "string") {
+    return res.status(400).send("'idToDelete' deve ser uma string");
+  }
+
   const studentIndex = students.findIndex(
     (student) => student.id === idToDelete
   );
@@ -108,10 +160,16 @@ app.delete("/students/:id", (req: Request, res: Response) => {
 
   res.status(200).send("Item deletado com sucesso");
 });
-
 app.post("/notes/:id", (req: Request, res: Response) => {
   const idStudent = req.params.id;
   const newNotes = req.body.notes as string;
+
+  if (typeof idStudent !== "string") {
+    return res.status(400).send("'idStudent' deve ser uma string");
+  }
+  if (typeof newNotes !== "string") {
+    return res.status(400).send("'newNotes' deve ser uma string");
+  }
 
   const resultStudent = students.find((student) => student.id === idStudent);
 
