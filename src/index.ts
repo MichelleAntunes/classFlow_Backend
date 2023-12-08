a07cd299a673c956f020d15a22a9438b408afc3f
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-08 19:39:14 +0100

    Reorganised the checking of data received in the backend more efficiently by using tray catch for each check.

--- src/index.ts
+++ src/index.ts
@@ -36,150 +36,241 @@ app.get("/students", (req: Request, res: Response) => {
 });
 
 app.post("/students", (req: Request, res: Response) => {
-  const id = req.body.id as string;
-  const name = req.body.name as string;
-  const age = req.body.age as number;
-  const email = req.body.email as string;
-  const telepfone = req.body.telepfone as number;
-  const notes = req.body.notes as string[];
+  try {
+    const id = req.body.id as string;
+    const name = req.body.name as string;
+    const age = req.body.age as number;
+    const email = req.body.email as string;
+    const telepfone = req.body.telepfone as number;
+    const notes = req.body.notes as string[];
 
-  if (typeof id !== "string") {
-    return res.status(400).send("'id' deve ser uma string");
-  }
-  if (typeof name !== "string") {
-    return res.status(400).send("'name' deve ser uma string");
-  }
-  if (typeof age !== "number") {
-    return res.status(400).send("'age' deve ser uma number");
-  }
-  if (typeof email !== "string") {
-    return res.status(400).send("'email' deve ser uma string");
-  }
-  if (typeof telepfone !== "number") {
-    return res.status(400).send("'telepfone' deve ser uma number");
-  }
-  if (typeof notes !== "string") {
-    return res.status(400).send("'notes' deve ser uma string");
-  }
+    if (id !== undefined) {
+      if (typeof id !== "string" || id.length < 1) {
+        throw new Error(
+          "'id' deve ser uma string e ser maior que um caractere"
+        );
+      }
+    }
+    if (name !== undefined) {
+      if (typeof name !== "string" || name.length < 2) {
+        throw new Error(
+          "'name' deve ser uma string e ter mais de dois caracteres"
+        );
+      }
+    }
+    if (age !== undefined) {
+      if (typeof age !== "number") {
+        throw new Error("'age' deve ser uma number");
+      }
+    }
 
-  const newStudent: TStudents = {
-    id,
-    name,
-    age,
-    email,
-    telepfone,
-    notes,
-  };
+    if (email !== undefined) {
+      if (typeof email !== "string") {
+        throw new Error("'email' deve ser uma string");
+      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        return res.status(400).send("'newEmail' deve ser um email válido");
+      }
+    }
 
-  students.push(newStudent);
+    if (telepfone !== undefined) {
+      if (typeof telepfone !== "number") {
+        throw new Error("'telepfone' deve ser uma number");
+      }
+      const newTelephoneString: string = telepfone.toString();
+      const phoneRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
-  res.status(201).send("Cadastro do novo aluno realizado com sucesso");
+      if (!phoneRegex.test(newTelephoneString)) {
+        return res
+          .status(400)
+          .send("'newTelephone' deve ser um número de telefone válido");
+      }
+    }
+    if (notes !== undefined) {
+      if (typeof notes !== "string") {
+        throw new Error("'notes' deve ser uma string");
+      }
+    }
+
+    const newStudent: TStudents = {
+      id,
+      name,
+      age,
+      email,
+      telepfone,
+      notes,
+    };
+
+    students.push(newStudent);
+    res.status(201).send("Cadastro do novo aluno realizado com sucesso");
+  } catch (error: any) {
+    res.status(400).send(error.message);
+  }
 });
 
 app.get("/students/:id", (req: Request, res: Response) => {
-  //:id poderia ser qualquer outro filtro necessário
-  const idToFind = req.params.id; // não precisamos forçar a tipagem aqui, porque todo path params é string
+  try {
+    //:id poderia ser qualquer outro filtro necessário
+    const idToFind = req.params.id; // não precisamos forçar a tipagem aqui, porque todo path params é string
 
-  if (typeof idToFind !== "string") {
-    return res.status(400).send("'idToFind' deve ser uma string");
+    if (typeof idToFind !== "string") {
+      return res.status(400).send("'idToFind' deve ser uma string");
+    }
+
+    const result = students.find((student) => student.id === idToFind);
+
+    if (!result) {
+      return res.status(404).send("Estudante não encontrado");
+    }
+    res.status(200).send(result);
+    // .send('O estudante localizado foi: ${result} seu nome é ${result?.name}');
+  } catch (error: any) {
+    res.status(400).send(error.message);
   }
-
-  const result = students.find((student) => student.id === idToFind);
-
-  res.status(200).send(result);
-  // .send('O estudante localizado foi: ${result} seu nome é ${result?.name}');
 });
-
+//edit
 app.put("/students/:id", (req: Request, res: Response) => {
-  const idToEdit = req.params.id;
-  if (typeof idToEdit !== "string") {
-    return res.status(400).send("'id' deve ser uma string");
-  }
+  try {
+    const idToEdit = req.params.id;
 
-  const newId = req.body.id as string | undefined;
-  const newName = req.body.name as string | undefined;
-  const newEmail = req.body.email as string | undefined;
-  const newAge = req.body.age as number | undefined;
-  const newTelephone = req.body.telepfone as number | undefined;
-  const newNotes = req.body.notes as string[] | undefined;
+    if (typeof idToEdit !== "string") {
+      return res.status(400).send("'id' deve ser uma string");
+    }
 
-  if (typeof newId !== "string") {
-    return res.status(400).send("'newId' deve ser uma string");
-  }
-  if (typeof newName !== "string") {
-    return res.status(400).send("'newName' deve ser uma string");
-  }
-  if (typeof newEmail !== "string") {
-    return res.status(400).send("'newEmail' deve ser uma string");
-  }
-  if (typeof newAge !== "number") {
-    return res.status(400).send("'newAge' deve ser uma number");
-  }
-  if (typeof newTelephone !== "number") {
-    return res.status(400).send("'newTelephone' deve ser uma number");
-  }
-  if (typeof newNotes !== "string") {
-    return res.status(400).send("'newNotes' deve ser uma number");
-  }
+    const student = students.find((student) => student.id === idToEdit);
 
-  const student = students.find((student) => student.id === idToEdit);
+    if (!student) {
+      return res.status(404).send("Estudante não encontrado");
+    }
 
-  if (student) {
-    student.id = newId || student.id;
-    student.name = newName || student.name;
-    student.email = newEmail || student.email;
-    student.age = newAge || student.age;
-    student.telepfone = newTelephone || student.telepfone;
-    student.notes = newNotes || student.notes;
+    const newId = req.body.id as string | undefined;
+    const newName = req.body.name as string | undefined;
+    const newEmail = req.body.email as string | undefined;
+    const newAge = req.body.age as number | undefined;
+    const newTelephone = req.body.telephone as number | undefined;
+    const newNotes = req.body.notes as string[] | undefined;
 
-    // quando o valor for um número, é possível que seja 0 (que também é falsy)
-    // então para possibilitar que venha 0, podemos fazer um ternário
-    // o isNaN é uma função que checa se o argumento é um número ou não
-    // caso não seja um número o isNaN retorna true, caso contrário false
-    // por isso mantemos o antigo (pet.age) no true e atualizamos no false
-    student.age = isNaN(Number(newAge)) ? student.age : (newAge as number);
+    if (newId !== undefined) {
+      if (typeof newId !== "string" || newId.length < 1) {
+        return res
+          .status(400)
+          .send("'newId' deve ser uma string e ter pelo menos um caractere");
+      }
+    }
+
+    if (newName !== undefined) {
+      if (typeof newName !== "string" || newName.length < 2) {
+        return res
+          .status(400)
+          .send(
+            "'newName' deve ser uma string e ter pelo menos dois caracteres"
+          );
+      }
+    }
+
+    if (newEmail !== undefined) {
+      if (typeof newEmail !== "string") {
+        return res.status(400).send("'newEmail' deve ser uma string");
+      }
+
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(newEmail)) {
+        return res.status(400).send("'newEmail' deve ser um email válido");
+      }
+    }
+    if (newAge !== undefined) {
+      if (typeof newAge !== "number") {
+        return res.status(400).send("'newAge' deve ser um número");
+      }
+    }
+
+    if (newTelephone !== undefined) {
+      if (typeof newTelephone !== "number") {
+        return res.status(400).send("'newTelephone' deve ser um número");
+      }
+
+      const newTelephoneString: string = newTelephone.toString();
+      const phoneRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
+
+      if (!phoneRegex.test(newTelephoneString)) {
+        return res
+          .status(400)
+          .send("'newTelephone' deve ser um número de telefone válido");
+      }
+    }
+
+    if (newNotes !== undefined) {
+      if (typeof newNotes !== "string") {
+        return res.status(400).send("'newNotes' deve ser uma string");
+      }
+    }
+
+    res.status(200).send("Atualização realizada com sucesso");
+  } catch (error: any) {
+    res.status(400).send(error.message);
   }
-
-  res.status(200).send("Atualização realizada com sucesso");
 });
 
 app.delete("/students/:id", (req: Request, res: Response) => {
-  const idToDelete = req.params.id;
+  try {
+    const idToDelete = req.params.id;
 
-  if (typeof idToDelete !== "string") {
-    return res.status(400).send("'idToDelete' deve ser uma string");
-  }
+    if (typeof idToDelete !== "string") {
+      return res.status(400).send("'idToDelete' deve ser uma string");
+    }
 
-  const studentIndex = students.findIndex(
-    (student) => student.id === idToDelete
-  );
+    const studentIndex = students.findIndex(
+      (student) => student.id === idToDelete
+    );
 
-  if (studentIndex >= 0) {
+    if (studentIndex === -1) {
+      return res.status(404).send("Estudante não encontrado");
+    }
+
     students.splice(studentIndex, 1);
-  }
 
-  res.status(200).send("Item deletado com sucesso");
+    res.status(204).send();
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 });
+
 app.post("/notes/:id", (req: Request, res: Response) => {
-  const idStudent = req.params.id;
-  const newNotes = req.body.notes as string;
+  try {
+    const idStudent = req.params.id;
+    const newNotes = req.body.notes as string;
 
-  if (typeof idStudent !== "string") {
-    return res.status(400).send("'idStudent' deve ser uma string");
+    if (typeof idStudent !== "string") {
+      return res.status(400).send("'idStudent' deve ser uma string");
+    }
+    if (typeof newNotes !== "string") {
+      return res.status(400).send("'newNotes' deve ser uma string");
+    }
+
+    const resultStudent = students.find((student) => student.id === idStudent);
+
+    if (!resultStudent) {
+      return res.status(404).send("Estudante não encontrado");
+    }
+
+    if (resultStudent) {
+      resultStudent.notes = Array.isArray(resultStudent.notes)
+        ? resultStudent.notes
+        : [resultStudent.notes];
+
+      resultStudent.notes.push(newNotes);
+    }
+    // resultStudent.notes = Array.isArray(resultStudent.notes)
+    //   ? [...resultStudent.notes, newNotes]
+    //   : [newNotes];
+
+    // resultStudent.notes = (Array.isArray(resultStudent.notes)
+    // ? resultStudent.notes.concat(newNotes)
+    // : [newNotes]) as string[];
+
+    res.status(200).send("Novo comentário adicionado com sucesso");
+  } catch (error: any) {
+    res.status(400).send(error.message);
   }
-  if (typeof newNotes !== "string") {
-    return res.status(400).send("'newNotes' deve ser uma string");
-  }
-
-  const resultStudent = students.find((student) => student.id === idStudent);
-
-  if (resultStudent) {
-    resultStudent.notes = Array.isArray(resultStudent.notes)
-      ? resultStudent.notes
-      : [resultStudent.notes];
-
-    resultStudent.notes.push(newNotes);
-  }
-
-  res.status(200).send("Novo comentário adicionado com sucesso");
 });
