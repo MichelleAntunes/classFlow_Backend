2fa75298d017a572f91480b431fdfd07770a89ed
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-12 19:04:31 +0100

    Insert example of availability for teacher t01 and translate all the comments from Portuguese into English.

--- classFlow.sql
+++ classFlow.sql
@@ -60,27 +60,55 @@ CREATE TABLE chat (
 );
 DROP TABLE chat; 
 
+-- Table inactive_students: Stores information about inactive students.
+CREATE TABLE inactive_students (
+  id TEXT PRIMARY KEY NOT NULL,
+  name TEXT NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  phone INTEGER,
+  age INTEGER,
+  notes TEXT,
+  teacher_id TEXT NOT NULL,
+  photo BLOB,
+  FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+);
+
+DROP TABLE inactive_students; 
+
+-- Table to store the calendar with the teacher's availability
+
+CREATE TABLE calendar (
+  id INTEGER PRIMARY KEY,
+  teacher_id TEXT NOT NULL,
+  day_of_week INTEGER NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+  FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+);
+
+DROP TABLE calendar; 
+
 INSERT INTO teacher (id, name, email, password) VALUES ('t01', 'Professor Smith', 'prof.smith@example.com', 'password123');
 
--- Inserir alunos com IDs fictícios 's01', 's02' e 's03', associados ao professor 't01'
+-- Insert students with fictitious IDs 's01', 's02' and 's03', associated with teacher 't01'
 INSERT INTO students (id, name, email, phone, age, notes, teacher_id, photo) VALUES
   ('s01', 'Student A', 'studentA@example.com', 123456789, 20, 'Note for Student A', 't01', x'F1F2F3F4'),
   ('s02', 'Student B', 'studentB@example.com', 987654321, 22, 'Note for Student B', 't01', x'F5F6F7F8'),
   ('s03', 'Student C', 'studentC@example.com', 555666777, 25, 'Note for Student C', 't01', x'F9FAFBFC');
 
--- Inserir relações professor-aluno
+-- Inserting teacher-student relationships
 INSERT INTO professor_student_relationship (teacher_id, student_id) VALUES
   ('t01', 's01'),
   ('t01', 's02'),
   ('t01', 's03');
 
--- Inserir notas associadas aos alunos
+-- Insert grades associated with students
 INSERT INTO notes (student_id, teacher_id, note) VALUES
   ('s01', 't01', 'Note 1 for Student A'),
   ('s02', 't01', 'Note 1 for Student B'),
   ('s03', 't01', 'Note 1 for Student C');
 
--- Inserir mensagens de chat entre professor e alunos
+-- Insert chat messages between teacher and students
 INSERT INTO chat (student_id, teacher_id, message) VALUES
   ('s01', 't01', 'Hello Student A, how are you?'),
   ('s02', 't01', 'Hi Student B, did you understand the lecture?'),
@@ -88,15 +116,15 @@ INSERT INTO chat (student_id, teacher_id, message) VALUES
 
 DROP TABLE students; -- Exemple
 
---Lista de Alunos do Professor:
--- Substitua 't01' pelo ID do professor logado
+--Teacher's Student List:
+--Replace 't01' with the logged in teacher's ID
 SELECT students.id, students.name
 FROM students
 JOIN professor_student_relationship ON students.id = professor_student_relationship.student_id
 WHERE professor_student_relationship.teacher_id = 't01';
 
---Detalhes do Aluno Selecionado:
--- Substitua 't01' pelo ID do professor logado e 's01' pelo ID do aluno selecionado
+--Selected Student Details:
+--Replace 't01' with the logged in teacher ID and 's01' with the selected student ID
 SELECT students.name, students.phone, students.email, students.notes, students.photo
 FROM students
 JOIN professor_student_relationship ON students.id = professor_student_relationship.student_id
@@ -104,14 +132,23 @@ WHERE professor_student_relationship.teacher_id = 't01'
   AND students.id = 's01';
 
 
- --Incluir Novo Aluno:
- -- Substitua 't01' pelos dados do professor e forneça os valores apropriados para o novo aluno
+ --Include New Student:
+ -- Replace 't01' with the teacher's data and provide the appropriate values for the new student
 INSERT INTO students (id, name, email, phone, age, notes, teacher_id)
 VALUES ('s04', 'Novo Aluno', 'novo.aluno@example.com', 123456789, 20, 'Notas do Novo Aluno', 't01');
 
--- Substitua 't01' pelo ID do professor logado e forneça os novos valores apropriados
+-- Replace 't01' with the ID of the logged-in teacher and provide the appropriate new values
 UPDATE teacher
 SET name = 'Novo Nome',
     email = 'novo.email@example.com',
     password = 'nova_senha'
 WHERE id = 't01';
+
+-- Move student to the inactive students table
+INSERT INTO inactive_students SELECT * FROM students WHERE id = 's01';
+DELETE FROM students WHERE id = 's01';
+
+-- Insert availability example for teacher 't01'
+VALUES
+  ('t01', 1, '09:00:00', '12:00:00'), -- Segunda-feira, 9h às 12h
+  ('t01', 3, '14:00:00', '18:00:00'); -- Quarta-feira, 14h às 18h
