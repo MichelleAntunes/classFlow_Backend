9f14dae68750bc88afd8fd38c76232df3f2c4492
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-13 17:38:59 +0100

    Redid the deletion of a student from the student list, after he was included in the list of inactive students using fewer DELETE commands and using ON DELETE CASCADE where there was FK.

--- classFlow.sql
+++ classFlow.sql
@@ -8,7 +8,7 @@ CREATE TABLE teacher (
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
-DROP TABLE teacher; -- Exemple
+-- DROP TABLE teacher; 
 
 -- Table class: Represents classes or lists of students associated with a teacher.
 CREATE TABLE class (
@@ -17,7 +17,7 @@ CREATE TABLE class (
   teacher_id TEXT NOT NULL,
   FOREIGN KEY (teacher_id) REFERENCES teacher (id)
 );
-DROP TABLE class;
+-- DROP TABLE class;
 
 -- Table students (alunos): Stores information about students, including a foreign key teacher_id that references the teacher table to indicate which teacher is associated with each student.
 CREATE TABLE students (
@@ -33,42 +33,41 @@ CREATE TABLE students (
     class_id TEXT NOT NULL,
   FOREIGN KEY (class_id) REFERENCES class (id),
   FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+  
 );
 
 -- Table professor_student_relationship: Establishes a many-to-many relationship between teachers and students, allowing a teacher to have multiple students and a student to have multiple teachers.
 CREATE TABLE professor_student_relationship (
-    
-  teacher_id TEXT NOT NULL,
+      teacher_id TEXT NOT NULL,
   student_id TEXT NOT NULL,
   PRIMARY KEY (teacher_id, student_id),
   FOREIGN KEY (teacher_id) REFERENCES teacher (id),
-  FOREIGN KEY (student_id) REFERENCES students (id)
+  FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE
 );
+-- DROP TABLE professor_student_relationship; 
 
-DROP TABLE professor_student_relationship; 
 -- Table notes: Stores notes associated with students, with foreign keys student_id and teacher_id referencing the students and teachers tables, respectively.
 CREATE TABLE notes (
   id INTEGER PRIMARY KEY,
-  student_id INTEGER,
+  student_id TEXT,
   teacher_id TEXT,
-  note TEXT NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES students (id),
-  FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+  note TEXT NOT NULL,  
+  FOREIGN KEY (teacher_id) REFERENCES teacher (id),
+  FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE
 );
-
-DROP TABLE notes; 
+-- DROP TABLE notes; 
 
 -- Table chat: Stores chat messages between teachers and students, with foreign keys student_id and teacher_id referencing the students and teachers tables, respectively.
 CREATE TABLE chat (
   id INTEGER PRIMARY KEY,
-  student_id INTEGER,
+  student_id TEXT,
   teacher_id TEXT,
   message TEXT NOT NULL,
-  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (student_id) REFERENCES students (id),
-  FOREIGN KEY (teacher_id) REFERENCES teacher (id)
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+  FOREIGN KEY (teacher_id) REFERENCES teacher (id),
+  FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE
 );
-DROP TABLE chat; 
+-- DROP TABLE chat; 
 
 -- Table inactive_students: Stores information about inactive students.
 CREATE TABLE inactive_students (
@@ -82,15 +81,12 @@ CREATE TABLE inactive_students (
   photo BLOB DEFAULT x'F1F2F3F4', -- Default value for a dummy image (change as necessary)
   teacher_id TEXT NOT NULL,
     class_id TEXT NOT NULL,
-    
-  FOREIGN KEY (class_id) REFERENCES class (id),
+      FOREIGN KEY (class_id) REFERENCES class (id),
   FOREIGN KEY (teacher_id) REFERENCES teacher (id)
 );
-
-DROP TABLE inactive_students; 
+-- DROP TABLE inactive_students; 
 
 -- Table to store the calendar with the teacher's availability
-
 CREATE TABLE calendar (
   id INTEGER PRIMARY KEY,
   teacher_id TEXT NOT NULL,
@@ -99,21 +95,27 @@ CREATE TABLE calendar (
   end_time TIME NOT NULL,
   FOREIGN KEY (teacher_id) REFERENCES teacher (id)
 );
+-- DROP TABLE calendar; 
 
-DROP TABLE calendar; 
-
-CREATE TABLE password_reset (
+-- Table password_reset teachers
+CREATE TABLE password_reset_teacher (
   id INTEGER PRIMARY KEY,
   user_id TEXT NOT NULL,
-  user_type TEXT NOT NULL, -- 'teacher' ou 'student'
   token TEXT NOT NULL,
   expires_at TIMESTAMP NOT NULL,
-  FOREIGN KEY (user_id, user_type) REFERENCES teacher (id, 'teacher') ON DELETE CASCADE,
-  FOREIGN KEY (user_id, user_type) REFERENCES students (id, 'student') ON DELETE CASCADE
+  FOREIGN KEY (user_id) REFERENCES teacher (id) ON DELETE CASCADE
 );
+--  DROP TABLE password_reset_teacher; 
 
-
-DROP TABLE password_reset; 
+ -- Table password_reset students
+CREATE TABLE password_reset_students (
+  id INTEGER PRIMARY KEY,
+  user_id TEXT NOT NULL,
+  token TEXT NOT NULL,
+  expires_at TIMESTAMP NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES students (id) ON DELETE CASCADE
+);
+--  DROP TABLE password_reset_students; 
 
 -- Table annotations: Stores annotations associated with students, with foreign keys student_id and teacher_id referencing the students and teachers tables, respectively.
 CREATE TABLE annotations (
@@ -121,19 +123,24 @@ CREATE TABLE annotations (
   student_id TEXT,
   teacher_id TEXT,
   annotation TEXT NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES students (id),
+  FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE,
   FOREIGN KEY (teacher_id) REFERENCES teacher (id)
 );
+-- DROP TABLE annotations; 
 
-DROP TABLE annotations; 
 INSERT INTO teacher (id, name, email, password) VALUES ('t01', 'Professor Smith', 'prof.smith@example.com', 'password123');
+
+ -- Insert values into the class table to represent different classes
+INSERT INTO class (id, name, teacher_id) VALUES
+  ('c03', 'English Class', 't01'),
+  ('c04', 'French Class', 't01');
 
 -- Insert students with fictitious IDs 's01', 's02' and 's03', associated with teacher 't01'
 INSERT INTO students (id, name, email, phone, age, notes, annotations, teacher_id, class_id, photo)
 VALUES
-  ('s01', 'Student A', 'studentA@example.com', 123456789, 20, '', '', 't01', 'c01', x'F1F2F3F4'),
-  ('s02', 'Student B', 'studentB@example.com', 987654321, 22, '', '', 't01', 'c01', x'F5F6F7F8'),
-  ('s03', 'Student C', 'studentC@example.com', 555666777, 25, '', '', 't01', 'c02', x'F9FAFBFC');
+  ('s01', 'Student A', 'studentA@example.com', 123456789, 20, '', '', 't01', 'c03', x'F1F2F3F4'),
+  ('s02', 'Student B', 'studentB@example.com', 987654321, 22, '', '', 't01', 'c03', x'F5F6F7F8'),
+  ('s03', 'Student C', 'studentC@example.com', 555666777, 25, '', '', 't01', 'c04', x'F9FAFBFC');
 
 
 -- Inserting teacher-student relationships
@@ -148,13 +155,32 @@ INSERT INTO notes (student_id, teacher_id, note) VALUES
   ('s02', 't01', 'Note 1 for Student B'),
   ('s03', 't01', 'Note 1 for Student C');
 
+  -- Inserting annotations associated with students
+-- Replace 't01' and 's01' with the teacher and student IDs, and provide the appropriate annotation text
+INSERT INTO annotations (student_id, teacher_id, annotation) VALUES
+  ('s01', 't01', 'Annotation 1 for Student A'),
+  ('s02', 't01', 'Annotation 1 for Student B'),
+  ('s03', 't01', 'Annotation 1 for Student C');
+
 -- Insert chat messages between teacher and students
 INSERT INTO chat (student_id, teacher_id, message) VALUES
   ('s01', 't01', 'Hello Student A, how are you?'),
   ('s02', 't01', 'Hi Student B, did you understand the lecture?'),
   ('s03', 't01', 'Student C, please review the assignment.');
 
-DROP TABLE students; -- Exemple
+-- Move student to the inactive students table
+INSERT INTO inactive_students SELECT * FROM students WHERE id = 's01';
+
+-- Now, delete the student from the students table
+DELETE FROM students WHERE id = 's01';
+
+SELECT * FROM students WHERE id = 's01';
+
+-- Insert availability example for teacher 't01'
+INSERT INTO calendar (teacher_id, day_of_week, start_time, end_time)
+VALUES
+  ('t01', 1, '09:00:00', '12:00:00'), -- Segunda-feira, 9h às 12h
+  ('t01', 3, '14:00:00', '18:00:00'); -- Quarta-feira, 14h às 18h
 
 --Teacher's Student List:
 --Replace 't01' with the logged in teacher's ID
@@ -172,7 +198,6 @@ WHERE professor_student_relationship.teacher_id = 't01'
   AND students.id = 's01';
 
 
-
 -- Replace 't01' with the ID of the logged-in teacher and provide the appropriate new values
 UPDATE teacher
 SET name = 'Novo Nome',
@@ -180,44 +205,7 @@ SET name = 'Novo Nome',
     password = 'nova_senha'
 WHERE id = 't01';
 
-SELECT * FROM students WHERE id = 's01';
-
--- Move student to the inactive students table
-INSERT INTO inactive_students SELECT * FROM students WHERE id = 's01';
-
--- Delete professor-student relationships
-DELETE FROM professor_student_relationship WHERE student_id = 's01';
-
--- Delete notes associated with the student
-DELETE FROM notes WHERE student_id = 's01';
-
--- Delete chat messages associated with the student
-DELETE FROM chat WHERE student_id = 's01';
-
--- Delete annotations associated with the student
-DELETE FROM annotations WHERE student_id = 's01';
-
--- Now, delete the student from the students table
-DELETE FROM students WHERE id = 's01';
-
 SELECT * FROM inactive_students; 
 DELETE FROM inactive_students;
 
-SELECT * FROM students;
 
--- Insert availability example for teacher 't01'
-VALUES
-  ('t01', 1, '09:00:00', '12:00:00'), -- Segunda-feira, 9h às 12h
-  ('t01', 3, '14:00:00', '18:00:00'); -- Quarta-feira, 14h às 18h
-
--- Inserting annotations associated with students
--- Replace 't01' and 's01' with the teacher and student IDs, and provide the appropriate annotation text
-INSERT INTO annotations (student_id, teacher_id, annotation) VALUES
-  ('s01', 't01', 'Annotation 1 for Student A'),
-  ('s02', 't01', 'Annotation 1 for Student B'),
-  ('s03', 't01', 'Annotation 1 for Student C');
-
-  -- Insert values into the class table to represent different classes
-INSERT INTO class (id, name, teacher_id) VALUES
-  ('c03', 'English Class', 't01'),
-  ('c04', 'French Class', 't01');
