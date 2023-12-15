daab1f22f3ae725ed9b59fed29e7557e7be5fb64
Michelle Antunes <117948294+MichelleAntunes@users.noreply.github.com>
2023-12-15 16:19:10 +0100

    Updated grade typing so that a student can have an array with several pieces of information.

--- src/types.ts
+++ src/types.ts
@@ -5,14 +5,18 @@ export type TImageData = {
   data: Buffer;
   mimeType: "image/png" | "image/jpeg";
 };
-
+export type TNote = {
+  id: string; // Add a unique identifier for each note
+  note: string;
+  // Add any other fields relevant to the note here
+};
 export type TStudents = {
   id: string;
   name: string;
   email: string;
   phone: number | null;
   age: number | null;
-  notes?: number[];
+  notes?: TNote[]; // Now it's an array of objects
   annotations?: string[];
   photo?: TImageData | string | null;
   teacher_id: string;
